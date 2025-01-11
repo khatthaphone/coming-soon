3912fb006ea9f221bfb0e6d337ac0546f27a76e3
Khatthaphone <8247636+khatthaphone@users.noreply.github.com>
2025-01-11 17:47:35 +0700

    Update SEO

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,6 +1,6 @@
 export const settings = {
-  site: 'https://cannonball.littlesticks.dev',
-  name: 'Little Sticks',
-  title: 'Cannonball by Little Sticks | A splash page template',
-  description: 'This is a simple splash page template built with Astro by Little Sticks',
+  site: 'https://online-qr.com',
+  name: 'Online QR',
+  title: 'Online QR Generator & Endpoint | QR with a single line of code',
+  description: 'Easily generate your custom QR Code | Download or add to your site/app with <img /> or a single line of code',
 }
