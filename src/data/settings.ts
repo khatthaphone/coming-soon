0dec08e5aa893f6eb5ddd4129c064b9166ff0974
Khatthaphone <8247636+khatthaphone@users.noreply.github.com>
2025-01-13 10:50:04 +0700

    Add metadata

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,6 +1,6 @@
 export const settings = {
   site: 'https://online-qr.com',
   name: 'Online QR',
-  title: 'Online QR Generator & Endpoint | QR with a single line of code',
-  description: 'Easily generate your custom QR Code | Download or add to your site/app with <img /> or a single line of code',
+  title: 'Online QR Generator & API - Create Custom QR Codes Easily',
+  description: 'Easily generate custom QR codes with our Online QR Generator & API. Download or integrate into your site/app with a single line of code. Perfect for developers, marketers and businesses.',
 }
